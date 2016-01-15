f40b210f487004c5f2797937296f3bc5634977e8
Evan Kincade <demosthenes.evan@gmail.com>
2016-01-15 15:32:37 -0800

    Upload

--- saas_app/config/environment.rb
+++ saas_app/config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :authentication => :plain,
+    :user_name => ENV['app46038588@heroku.com'],
+    :password => ENV['awxyysut0509'],
+    :domain => 'damp-beyond-2080.herokuapp.com',
+    :enable_startstls_auto => true
+}
