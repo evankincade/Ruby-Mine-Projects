ff6073432ce805b7bfe067381f70fcc495d6398d
Evan Kincade <demosthenes.evan@gmail.com>
2016-01-24 12:25:58 -0800

    Update environment.rb

--- saas_app/config/environment.rb
+++ saas_app/config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
-    :user_name => ENV['app46038588@heroku.com'],
-    :password => ENV['awxyysut0509'],
+    :user_name => ENV[''],
+    :password => ENV[''],
     :domain => 'damp-beyond-2080.herokuapp.com',
     :enable_startstls_auto => true
 }
